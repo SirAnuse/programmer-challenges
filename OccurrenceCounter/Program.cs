9e927ff2ae97f90618c9e0e5c6f45613ef8f0dfc
Rustiniz <12233202+SirAnuse@users.noreply.github.com>
2020-02-28 23:32:55 +1100

    Unsabotage

--- OccurrenceCounter/Program.cs
+++ OccurrenceCounter/Program.cs
@@ -14,7 +14,11 @@ namespace OccurrenceCounter
             {
                 Console.Write("Please enter a string to count characters in: ");
                 input = Console.ReadLine();
-                if (String
+                if (String.IsNullOrWhiteSpace(input))
+                    Console.WriteLine("Input string cannot be empty.");
+                else
+                    break;
+            }
             
             Dictionary<char, int> dict = new Dictionary<char, int>();
 
